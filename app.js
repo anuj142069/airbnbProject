dd0d97f4571b6c8e973375063700bb96c60c6a2a
Anuj Kumar <anujkumar14102002@gmail.com>
2026-01-27 00:35:54 +0530

    Edit something in navbar

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const { default: mongoose } = require('mongoose');
 const multer = require('multer');
-
+const DB_PATH = "mongodb+srv://root:root@cluster0.mryid43.mongodb.net/airbnb";
 
 //Local Module
 const storeRouter = require("./routes/storeRouter")
